21d0ac499d2d2a631277910bf2659c542905e76d
Sean Anih <sean.anih@gmail.com>
2023-05-05 15:18:43 -0400

    successfully set up routing to another page and API route

---
+++ app/[id]/page.js
@@ -0,0 +1,5 @@
+function Page({ params }) {
+	return <div className='text-center'>Dynamic page number: {params.id}</div>;
+}
+
+export default Page;
---
+++ app/new-page/page.tsx
@@ -0,0 +1,4 @@
+function Page() {
+	return <div className='text-4xl text-center'>This worked</div>;
+}
+export default Page;
